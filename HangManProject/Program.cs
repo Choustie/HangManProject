abe74606e65cc4c9da1a1770dd30c5ebb8b171c1
Choustie <159010120+Choustie@users.noreply.github.com>
2025-09-03 11:14:18 +0200

    lieally did nothing
    🤑

---
+++ HangManProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HangManProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world!");
+        }
+    }
+}
