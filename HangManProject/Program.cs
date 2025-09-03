9e2bf88b81402320211fb59f4108abc697630724
Choustie <159010120+Choustie@users.noreply.github.com>
2025-09-03 12:02:33 +0200

    hangmanupdat1
    😂

--- HangManProject/Program.cs
+++ HangManProject/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace HangManProject
+﻿using System.Text;
+
+namespace HangManProject
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world!");
+            Console.WriteLine("Init HangMan");
+            var lifeLeft = 5;
+            var secretWord = "spel";
+            StringBuilder dislplayString = new StringBuilder();
+
+            for (int i = 0; i < secretWord.Length; i++)
+            {
+                dislplayString.Append('_');
+            }
+
+            Console.WriteLine(dislplayString.ToString());
+            //Console.WriteLine(Console.ReadLine());
         }
     }
 }
